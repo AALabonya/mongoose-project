933029ebc5d44b99e9e9ff6429e52d3357018ed6
AALabonya <1aklimakter@gmail.com>
2024-05-19 08:06:14 -0700

    added eslint and pretier

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,16 @@
-"use strict"
-// const express = require('express')
-import express, { Application, Request, Response } from "express"
-import cors from "cors"
-const app:Application = express()
-const port = 3000
+'use strict';
+import cors from 'cors';
+import express, { Application, Request, Response } from 'express';
 
-// parser 
-app.use(express.json())
-app.use(cors())
-app.get('/', (req:Request, res:Response) => {
-  res.send('Hello World!')
-})
+const app: Application = express();
+
+// parser
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req: Request, res: Response) => {
+  const a = 10;
+  res.send(a);
+});
 
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,17 @@
-import { error } from "console";
-import app from "./app";
-import config from "./app/config";
+import app from './app';
+import config from './app/config';
 
 // getting-started.js
-import mongoose from "mongoose";
-
+import mongoose from 'mongoose';
 
 async function main() {
- try{
+  try {
     await mongoose.connect(config.database_url as string);
     app.listen(config.port, () => {
-      console.log(`Example app listening on port ${config.port}`)
-    })
- }catch(error){
-console.log(error);
-
- }
+      console.log(`Example app listening on port ${config.port}`);
+    });
+  } catch (err) {
+    console.log(err);
+  }
 }
-
-
+main();
