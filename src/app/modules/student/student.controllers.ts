5e50db5aaa71ec2eea2eac268de76878b7ac02c8
AALabonya <1aklimakter@gmail.com>
2024-05-19 23:38:20 -0700

    added

--- src/app/modules/student/student.controllers.ts
+++ src/app/modules/student/student.controllers.ts
@@ -1,16 +1,26 @@
 import { Request, Response } from 'express';
 import { StudentService } from './student.service';
+import { log } from 'console';
 
 const createStudent = async (req: Request, res: Response) => {
-  const student = req.body;
+  try {
+    const student = req.body;
 
-  //will call service func to send this data
+    //will call service func to send this data
 
-  const result = await StudentService.createStudentIntoDB(student);
+    const result = await StudentService.createStudentIntoDB(student);
 
-  //send response
-  res.status(200).json({
-    success: true,
-    message: 'Student is created successfully',
-  });
+    //send response
+    res.status(200).json({
+      success: true,
+      message: 'Student is created successfully',
+      data: result,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const StudentControllers = {
+  createStudent,
 };
