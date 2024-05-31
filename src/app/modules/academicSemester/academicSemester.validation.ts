18f2c1d6bf99a712185015a528804ab62e04e806
AALabonya <1aklimakter@gmail.com>
2024-05-31 10:52:33 -0700

    new field added

--- src/app/modules/academicSemester/academicSemester.validation.ts
+++ src/app/modules/academicSemester/academicSemester.validation.ts
@@ -5,11 +5,13 @@ import {
   Months,
 } from './academicSemester.constant';
 
+
 const createAcademicSemesterValidationSchema = z.object({
   body: z.object({
     name: z.enum([...AcademicSemesterName] as [string, ...string[]]),
     year: z.string(),
     code: z.enum([...AcademicSemesterCode] as [string, ...string[]]),
+
     startMonth: z.enum([...Months] as [string, ...string[]]),
     endMonth: z.enum([...Months] as [string, ...string[]]),
   }),
