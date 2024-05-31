40f6a43ff489a9849f3a4ad3062c2e4330657e42
AALabonya <1aklimakter@gmail.com>
2024-05-30 23:17:06 -0700

    added service code

--- src/app/modules/academicSemester/academicSemester.service.ts
+++ src/app/modules/academicSemester/academicSemester.service.ts
@@ -1,8 +1,12 @@
 import { TAcademicSemesterCode } from './academicSemester.interface';
+import { academicSemester } from './academicSemester.model';
 
 const createAcademicsSemesterIntoBD = async (
   payload: TAcademicSemesterCode,
-) => {};
+) => {
+  const result = await academicSemester.create(payload);
+  return result;
+};
 
 export const AcademicSemesterServices = {
   createAcademicsSemesterIntoBD,
