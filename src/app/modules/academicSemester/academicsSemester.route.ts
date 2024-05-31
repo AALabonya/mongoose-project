f9b32b0e593d32e2bf9977ca3ba6a77963b3b881
AALabonya <1aklimakter@gmail.com>
2024-05-30 23:09:32 -0700

    added validation to  academicsSemester

--- src/app/modules/academicSemester/academicsSemester.route.ts
+++ src/app/modules/academicSemester/academicsSemester.route.ts
@@ -1,10 +1,15 @@
 import express from 'express';
 import { AcademicsSemesterControllers } from './academicsSemester.controllers';
+import { AcademicSemesterValidation } from './academicSemester.validation';
+import validateRequest from '../../middlwares/validateRequest';
 
 const router = express.Router();
 
 router.post(
   '/create-academic-semester',
+  validateRequest(
+    AcademicSemesterValidation.createAcademicSemesterValidationSchema,
+  ),
   AcademicsSemesterControllers.createAcademicsSemester,
 );
 
