cda34ba34f6e14df27cfe501a068487a2991e243
AALabonya <1aklimakter@gmail.com>
2024-05-30 23:55:42 -0700

    check relation

--- src/app/modules/academicSemester/academicSemester.service.ts
+++ src/app/modules/academicSemester/academicSemester.service.ts
@@ -4,6 +4,21 @@ import { academicSemester } from './academicSemester.model';
 const createAcademicsSemesterIntoBD = async (
   payload: TAcademicSemesterCode,
 ) => {
+  //here checking relation
+  //semester name --->semester code relation ase kina check korbo
+
+  type TAcademicsSemesterNameCodeMapper = {
+    Autumn: '01';
+    Summer: '02';
+    Fall: '03';
+  };
+
+  const academicsSemesterNameCodeMapper = {
+    Autumn: '01',
+    Summer: '02',
+    Fall: '03',
+  };
+
   const result = await academicSemester.create(payload);
   return result;
 };
