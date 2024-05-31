7e398e4623117eae5f3f84074ba09b17a616d3e5
AALabonya <1aklimakter@gmail.com>
2024-05-30 23:50:20 -0700

    added pre hook middleware

--- src/app/modules/academicSemester/academicSemester.model.ts
+++ src/app/modules/academicSemester/academicSemester.model.ts
@@ -38,6 +38,23 @@ const academicSemesterSchema = new Schema<TAcademicSemester>(
   },
 );
 
+//pre hook middleware use kore save korar ag muhurte document k atkai amra check korte pari
+//ai middleware a document ar access thakbe
+//se client theke data niye save korar ag muhurte check korbe
+
+//aki year a aki name 2ta ssemester create hobe na
+//normal function use korbo arrow function a this kaj korbe na
+
+academicSemesterSchema.pre('save', async function (next) {
+  const isSemesterExists = await academicSemester.findOne({
+    year: this.year,
+    name: this.name,
+  });
+  if (isSemesterExists) {
+    throw new Error('Semester is already exist !');
+  }
+});
+
 export const academicSemester = model<TAcademicSemester>(
   'AcademicSemester',
   academicSemesterSchema,
