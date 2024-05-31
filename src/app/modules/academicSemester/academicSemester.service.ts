6efe58f80a3726cf5547accab0883d15e638408e
AALabonya <1aklimakter@gmail.com>
2024-05-31 01:04:41 -0700

    updated code added to the services

--- src/app/modules/academicSemester/academicSemester.service.ts
+++ src/app/modules/academicSemester/academicSemester.service.ts
@@ -4,6 +4,7 @@ import { academicSemester } from './academicSemester.model';
 
 const createAcademicsSemesterIntoBD = async (payload: TAcademicSemester) => {
   //here maping chalabo data save korar age if block diye check korbo
+
   if (academicsSemesterNameCodeMapper[payload.name] !== payload.code) {
     throw new Error('Invalid Semester Code');
   }
@@ -20,9 +21,26 @@ const getSingleAcademicsSemesterIntoBD = async (id: string) => {
   const result = await academicSemester.findById(id);
   return result;
 };
+const updateAcademicsSemesterIntoBD = async (
+  id: string,
+  payload: Partial<TAcademicSemester>,
+) => {
+  if (
+    payload.name &&
+    payload.code &&
+    academicsSemesterNameCodeMapper[payload.name] !== payload.code
+  ) {
+    throw new Error('Invalid Semester Code');
+  }
 
+  const result = await academicSemester.findOneAndUpdate({ _id: id }, payload, {
+    new: true,
+  });
+  return result;
+};
 export const AcademicSemesterServices = {
   createAcademicsSemesterIntoBD,
   getAllAcademicsSemesterIntoBD,
   getSingleAcademicsSemesterIntoBD,
+  updateAcademicsSemesterIntoBD,
 };
