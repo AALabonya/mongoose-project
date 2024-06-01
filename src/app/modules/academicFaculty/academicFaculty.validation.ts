e1f27c275f7e1618b6abc61b3cc8c2406a887a62
AALabonya <1aklimakter@gmail.com>
2024-06-01 01:07:22 -0700

    added validation

--- src/app/modules/academicFaculty/academicFaculty.validation.ts
+++ src/app/modules/academicFaculty/academicFaculty.validation.ts
@@ -1,5 +1,7 @@
 import { z } from 'zod';
 
 const academicValidationSchema = z.object({
-  name: z.string,
+  name: z.string({
+    invalid_type_error: ' Academic faculty must be string',
+  }),
 });
