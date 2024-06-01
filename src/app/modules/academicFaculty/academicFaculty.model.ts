1219beef0dfcb7213ff11114aeb13397c1febe23
AALabonya <1aklimakter@gmail.com>
2024-06-01 00:59:18 -0700

    added

--- src/app/modules/academicFaculty/academicFaculty.model.ts
+++ src/app/modules/academicFaculty/academicFaculty.model.ts
@@ -14,4 +14,7 @@ const academicFacultySchema = new Schema<TAcademicFaculty>(
   },
 );
 
-export const AcademicFaculty = model;
+export const AcademicFaculty = model<TAcademicFaculty>(
+  'AcademicFaculty',
+  academicFacultySchema,
+);
