2ce3dd22f2187e6bc58ba9c2548e4aab9c309653
AALabonya <1aklimakter@gmail.com>
2024-06-01 01:43:49 -0700

    added

--- src/app/modules/academicFaculty/academicFaculty.route.ts
+++ src/app/modules/academicFaculty/academicFaculty.route.ts
@@ -1,14 +1,14 @@
 import express from 'express';
 import { AcademicFacultyControllers } from './academicFaculty.controller';
+import validateRequest from '../../middlwares/validateRequest';
+import { AcademicFacultyValidation } from './academicFaculty.validation';
 
 const router = express.Router();
 router.get('/:facultyId', AcademicFacultyControllers.createAcademicFaculty);
 router.post(
-  '/create-academic-semester',
-  validateRequest(
-    AcademicSemesterValidation.createAcademicSemesterValidationSchema,
-  ),
-  AcademicsSemesterControllers.createAcademicsSemester,
+  '/create-academic-faculty',
+  validateRequest(AcademicFacultyValidation.academicValidationSchema),
+  AcademicFacultyControllers.createAcademicFaculty,
 );
 
 router.patch(
