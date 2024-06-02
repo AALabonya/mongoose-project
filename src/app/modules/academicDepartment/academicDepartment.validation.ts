73c740eb20bb4e1fb38727f0dae1e2c8b1f2cf73
AALabonya <1aklimakter@gmail.com>
2024-06-01 20:23:51 -0700

    create academic Department

--- src/app/modules/academicDepartment/academicDepartment.validation.ts
+++ src/app/modules/academicDepartment/academicDepartment.validation.ts
@@ -4,7 +4,32 @@ const createAcademicDepartmentValidationSchema = z.object({
   body: z.object({
     name: z.string({
       invalid_type_error: 'Academic Department must be string',
+      required_error: 'Name is required',
     }),
-    academicFaculty:z.
+    academicFaculty: z.string({
+      invalid_type_error: 'Academic Department must be string',
+      required_error: 'Name is required',
+    }),
   }),
 });
+const updatedAcademicDepartmentValidationSchema = z.object({
+  body: z.object({
+    name: z
+      .string({
+        invalid_type_error: 'Academic Department must be string',
+        required_error: 'Name is required',
+      })
+      .optional(),
+    academicFaculty: z
+      .string({
+        invalid_type_error: 'Academic Department must be string',
+        required_error: 'Faculty is required',
+      })
+      .optional(),
+  }),
+});
+
+export const AcademicDepartmentValidation = {
+  createAcademicDepartmentValidationSchema,
+  updatedAcademicDepartmentValidationSchema,
+};
