592c140d8a731309e1ae31ad57227732a9b996d1
AALabonya <1aklimakter@gmail.com>
2024-06-01 20:18:01 -0700

    create academic Department

---
+++ src/app/modules/academicDepartment/academicDepartment.validation.ts
@@ -0,0 +1,10 @@
+import { z } from 'zod';
+
+const createAcademicDepartmentValidationSchema = z.object({
+  body: z.object({
+    name: z.string({
+      invalid_type_error: 'Academic Department must be string',
+    }),
+    academicFaculty:z.
+  }),
+});
