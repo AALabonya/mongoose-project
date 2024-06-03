4336808e22ed4bc9adfa78493669d66eb51fdd09
AALabonya <1aklimakter@gmail.com>
2024-06-03 14:07:37 -0700

    fixed issues

--- src/app/modules/student/student.controllers.ts
+++ src/app/modules/student/student.controllers.ts
@@ -2,6 +2,7 @@ import { StudentServices } from './student.service';
 import sendResponse from '../../../utils/sendResponse';
 import httpStatus from 'http-status';
 import catchAsync from '../../../utils/catchAsync';
+import mongoose from 'mongoose';
 
 const getSingleStudent = catchAsync(async (req, res) => {
   const { studentId } = req.params;
@@ -27,6 +28,10 @@ const getAllStudents = catchAsync(async (req, res) => {
 });
 
 const deleteStudent = catchAsync(async (req, res) => {
+  //user and student collection theke same data delete korar jonno transaction use korbo
+
+  const session = await mongoose.startSession();
+
   const { studentId } = req.params;
   const result = await StudentServices.deleteStudentFromDB(studentId);
 
