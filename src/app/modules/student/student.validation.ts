f3618c16c0158206f0ae29b3a74f018d6caa9919
AALabonya <1aklimakter@gmail.com>
2024-06-03 15:45:34 -0700

    added student update validation

--- src/app/modules/student/student.validation.ts
+++ src/app/modules/student/student.validation.ts
@@ -77,6 +77,29 @@ const updateLocalGuardianValidationSchema = z.object({
   contactNo: z.string().optional(),
   address: z.string().optional(),
 });
+
+export const updateStudentValidationSchema = z.object({
+  body: z.object({
+    password: z.string().max(20),
+    student: z.object({
+      name: createUserNameValidationSchema,
+      gender: z.enum(['male', 'female', 'other']),
+      dateOfBirth: z.string().optional(),
+      email: z.string().email(),
+      contactNo: z.string(),
+      emergencyContactNo: z.string(),
+      bloodGroup: z.enum(['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-']),
+      presentAddress: z.string(),
+      permanentAddress: z.string(),
+      guardian: createGuardianValidationSchema,
+      localGuardian: createLocalGuardianValidationSchema,
+      admissionSemester: z.string(),
+      profileImg: z.string(),
+    }),
+  }),
+});
+
 export const studentValidations = {
   createStudentValidationSchema,
+  updateStudentValidationSchema,
 };
