01d9bba1c2da0b494ac5f029dc7de2964af37583
AALabonya <1aklimakter@gmail.com>
2024-06-03 23:57:46 -0700

    error

--- src/app/middlwares/globalErrorHandler.ts
+++ src/app/middlwares/globalErrorHandler.ts
@@ -19,11 +19,16 @@ const globalErrorHandler = (err, req, res, next) => {
     path: '',
     message: 'Something went wrong',
   };
-const handleZodError=(err:ZodError)=>{
-  const errorSource=err.issues.map((issue:ZodIssue))
+  const handleZodError = (err: ZodError) => {
+    const errorSource = err.issues.map((issue: ZodIssue) => {
+      return {
+        path: issue?.path[issue.path.length - 1],
+        message: issue.message,
+      };
+    });
 
-  const statusCode =400
-}
+    const statusCode = 400;
+  };
   return res.status(statusCode).json({
     success: false,
     message,
