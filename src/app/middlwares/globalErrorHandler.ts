ca12494be5424a324f6e6aa16767c3ebb2492e44
AALabonya <1aklimakter@gmail.com>
2024-06-04 13:09:28 -0700

    error

--- src/app/middlwares/globalErrorHandler.ts
+++ src/app/middlwares/globalErrorHandler.ts
@@ -3,35 +3,21 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 //global error handler
 
-import { NextFunction, Request, Response } from 'express';
-import { ZodError, ZodIssue } from 'zod';
+import { ZodError } from 'zod';
 import { TErrorSource } from '../interface/error';
 import config from '../config';
+import handleZodError from '../errors/handleZodError';
 
 const globalErrorHandler = (err, req, res, next) => {
   let statusCode = err.statusCode || 500;
   let message = err.message || 'Something went wrong';
-  let errorSources: TErrorSources = [
+
+  let errorSources: TErrorSource = [
     {
       path: '',
       message: 'Something went wrong',
     },
   ];
-  const handleZodError = (err: ZodError) => {
-    const errorSources: TErrorSource = err.issues.map((issue: ZodIssue) => {
-      return {
-        path: issue?.path[issue.path.length - 1],
-        message: issue.message,
-      };
-    });
-
-    const statusCode = 400;
-    return {
-      statusCode,
-      message: 'Validation Error',
-      errorSources,
-    };
-  };
 
   if (err instanceof ZodError) {
     const simplifiedError = handleZodError(err);
