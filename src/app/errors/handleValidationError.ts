c317cf5ec196a15ce3134fb8a181defb6a430f35
AALabonya <1aklimakter@gmail.com>
2024-06-04 13:40:20 -0700

    error

---
+++ src/app/errors/handleValidationError.ts
@@ -0,0 +1,10 @@
+import mongoose from 'mongoose';
+
+const handleValidationError = (err: mongoose.Error.ValidationError) => {
+  const statusCode = 400;
+  return {
+    statusCode,
+    message: 'Validation Error',
+    errorSources,
+  };
+};
