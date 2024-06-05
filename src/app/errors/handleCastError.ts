9982c64fc5a8ceb6dd13a6514f12c5ab40fa701e
AALabonya <1aklimakter@gmail.com>
2024-06-04 17:32:32 -0700

    added validation error

---
+++ src/app/errors/handleCastError.ts
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose';
+import { TGenericErrorResponse } from '../interface/error';
+
+const handleCastError = (
+  err: mongoose.Error.CastError,
+): TGenericErrorResponse => {
+  const statusCode = 400;
+  return {
+    statusCode,
+    message: 'Validation Error',
+    errorSources,
+  };
+};
