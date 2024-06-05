395c209498d9d986fac863306c930bf4010206a2
AALabonya <1aklimakter@gmail.com>
2024-06-04 17:42:25 -0700

    added validation error

--- src/app/middlwares/globalErrorHandler.ts
+++ src/app/middlwares/globalErrorHandler.ts
@@ -36,6 +36,11 @@ const globalErrorHandler = (err, req, res, next) => {
     statusCode = simplifiedError?.statusCode;
     message = simplifiedError?.message;
     errorSources = simplifiedError?.errorSources;
+  } else if (err?.code === 11000) {
+    const simplifiedError = handleDuplicateError(err);
+    statusCode = simplifiedError?.statusCode;
+    message = simplifiedError?.message;
+    errorSources = simplifiedError?.errorSources;
   }
 
   //ultimate
