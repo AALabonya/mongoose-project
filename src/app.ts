8a518c615d74d72e380ea8c12744eceba66be44e
AALabonya <1aklimakter@gmail.com>
2024-06-04 18:25:03 -0700

    added duplicateError handler

--- src/app.ts
+++ src/app.ts
@@ -19,8 +19,9 @@ app.use(cors());
 app.use('/api/v1', router);
 
 const test = (req: Request, res: Response) => {
-  const a = 10;
-  res.send(a);
+  Promise.reject();
+  // const a = 10;
+  // res.send(a);
 };
 
 app.get('/', test);
--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,18 @@
+import { Server } from 'http';
 import app from './app';
 import config from './app/config';
 
 // getting-started.js
 import mongoose from 'mongoose';
 
+let server: Server;
+
 async function main() {
   try {
     await mongoose.connect(config.database_url as string);
     console.log('MongoDB Database Connected Successfully');
 
-    app.listen(config.port, () => {
+    server = app.listen(config.port, () => {
       console.log(`Example app listening on port ${config.port}`);
     });
   } catch (err) {
