3d0bd6cfbc962f7bbb6ccf95a686e7b4d85b69a9
AALabonya <1aklimakter@gmail.com>
2024-06-06 21:55:20 -0700

    remove hight

--- src/app/builder/QueryBuilder.ts
+++ src/app/builder/QueryBuilder.ts
@@ -3,4 +3,19 @@ import { Query } from 'mongoose';
 class QueryBuilder<T> {
   public modelQuery: Query<T[], T>;
   public query: Record<string, unknown>;
+
+  constructor(modelQuery: Query<T[], T>, query: Record<string, unknown>) {
+    this.modelQuery = modelQuery;
+    this.query = query;
+  }
+  search(searchableFields: string[]) {
+    if (this?.query?.searchTerm) {
+      //search query
+      this.model = this.modelQuery.find({
+        $or: searchableFields.map((field) => ({
+          [field]: { $regex: searchTerm, $options: 'i' },
+        })),
+      });
+    }
+  }
 }
