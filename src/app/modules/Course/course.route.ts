686b2f95dd913cf6a8f10af984d9c2da0b50d1a0
AALabonya <1aklimakter@gmail.com>
2024-06-07 21:11:54 -0700

    create course route

--- src/app/modules/Course/course.route.ts
+++ src/app/modules/Course/course.route.ts
@@ -2,6 +2,7 @@ import express from 'express';
 
 import { CourseControllers } from './course.controller';
 import { CourseValidations } from './course.validation';
+import validateRequest from '../../middlwares/validateRequest';
 
 const router = express.Router();
 
