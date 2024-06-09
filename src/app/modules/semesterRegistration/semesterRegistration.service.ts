f7b0911671d6152867aeeb5153a01274dd44d2c0
AALabonya <1aklimakter@gmail.com>
2024-06-09 00:36:09 -0700

    added semesterRegistration controller

--- src/app/modules/semesterRegistration/semesterRegistration.service.ts
+++ src/app/modules/semesterRegistration/semesterRegistration.service.ts
@@ -1,11 +1,17 @@
-const createSemesterRegistrationIntoDB = async () => {};
-const getAllSemesterRegistrationIntoDB = async () => {};
-const getSingleSemesterRegistrationIntoDB = async () => {};
+import { TSemesterRegistration } from './semesterRegistration.interface';
+
+const createSemesterRegistrationIntoDB = async (
+  payload: TSemesterRegistration,
+) => {
+  //check if the semester is exist
+};
+const getAllSemesterRegistrationFromDB = async () => {};
+const getSingleSemesterRegistrationFromDB = async () => {};
 const updateSemesterRegistrationIntoDB = async () => {};
 
 export const SemesterRegistrationService = {
   createSemesterRegistrationIntoDB,
-  getAllSemesterRegistrationIntoDB,
-  getSingleSemesterRegistrationIntoDB,
+  getAllSemesterRegistrationFromDB,
+  getSingleSemesterRegistrationFromDB,
   updateSemesterRegistrationIntoDB,
 };
