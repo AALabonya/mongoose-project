c461c2c0a6765760b21bb93621b278df9c424d75
AALabonya <1aklimakter@gmail.com>
2024-06-09 00:15:38 -0700

    added semesterRegistration controller

--- src/app/modules/semesterRegistration/semesterRegistration.controller.ts
+++ src/app/modules/semesterRegistration/semesterRegistration.controller.ts
@@ -1,6 +1,10 @@
 import { Request, Response } from 'express';
 import httpStatus from 'http-status';
 
+import catchAsync from '../../../utils/catchAsync';
+import sendResponse from '../../../utils/sendResponse';
+import { SemesterRegistrationService } from './semesterRegistration.service';
+
 const createSemesterRegistration = catchAsync(
   async (req: Request, res: Response) => {
     const result =
