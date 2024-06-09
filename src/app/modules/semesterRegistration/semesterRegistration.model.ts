22c5d23554ada7ae486b85f68ee195d1b56fdfa8
AALabonya <1aklimakter@gmail.com>
2024-06-09 00:11:53 -0700

    added semesterRegistration controller

--- src/app/modules/semesterRegistration/semesterRegistration.model.ts
+++ src/app/modules/semesterRegistration/semesterRegistration.model.ts
@@ -1,15 +1,43 @@
 import mongoose, { Schema } from 'mongoose';
+import { SemesterRegistrationStatus } from './semesterRegistration.constant';
 import { TSemesterRegistration } from './semesterRegistration.interface';
 
-const semesterRegistrationSchema = new mongoose.Schema<TSemesterRegistration>({
-  academicSemester: {
-    type: Schema.Types.ObjectId,
-    required: true,
-    unique: true,
-    ref: 'AcademicSemester',
+const semesterRegistrationSchema = new mongoose.Schema<TSemesterRegistration>(
+  {
+    academicSemester: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      unique: true,
+      ref: 'AcademicSemester',
+    },
+    status: {
+      type: String,
+      enum: SemesterRegistrationStatus,
+      default: 'UPCOMING',
+    },
+    startDate: {
+      type: Date,
+      required: true,
+    },
+    endDate: {
+      type: Date,
+      required: true,
+    },
+    minCredit: {
+      type: Number,
+      default: 3,
+    },
+    maxCredit: {
+      type: Number,
+      default: 15,
+    },
   },
-  status: {
-    type: String,
-    enum: ['UPCOMING', 'ONGOING', 'ENDED'],
+  {
+    timestamps: true,
   },
-});
+);
+
+export const SemesterRegistration = mongoose.model<TSemesterRegistration>(
+  'SemesterRegistration',
+  semesterRegistrationSchema,
+);
