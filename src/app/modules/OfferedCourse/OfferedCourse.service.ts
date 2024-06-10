40707f4e47ec28f103e50f2ca63cd980eb4926e8
AALabonya <1aklimakter@gmail.com>
2024-06-10 20:59:41 +0600

    added controllers code

--- src/app/modules/OfferedCourse/OfferedCourse.service.ts
+++ src/app/modules/OfferedCourse/OfferedCourse.service.ts
@@ -2,6 +2,18 @@ import { TOfferedCourse } from "./OfferedCourse.interface";
 import { OfferedCourse } from "./OfferedCourse.model";
 
 const createOfferedCourseIntoDB=async(payload:TOfferedCourse)=>{
+    const {
+        semesterRegistration,
+        academicFaculty,
+        academicDepartment,
+        course,
+        section,
+        faculty,
+        days,
+        startTime,
+        endTime,
+      } = payload;
+    
     const result =await OfferedCourse.create(payload)
     return result
 }
@@ -9,8 +21,5 @@ const createOfferedCourseIntoDB=async(payload:TOfferedCourse)=>{
 
 export const OfferedCourseServices = {
     createOfferedCourseIntoDB,
-    getAllOfferedCoursesFromDB,
-    getSingleOfferedCourseFromDB,
-    deleteOfferedCourseFromDB,
-    updateOfferedCourseIntoDB,
+    
   };
