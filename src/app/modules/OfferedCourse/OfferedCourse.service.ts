884294e4424dae3def274bc76af1fd808db10d8c
AALabonya <1aklimakter@gmail.com>
2024-06-10 21:03:55 +0600

    added controllers code

--- src/app/modules/OfferedCourse/OfferedCourse.service.ts
+++ src/app/modules/OfferedCourse/OfferedCourse.service.ts
@@ -1,5 +1,12 @@
+import httpStatus from "http-status";
+import AppError from "../../errors/AppError";
+import { SemesterRegistration } from "../semesterRegistration/semesterRegistration.model";
 import { TOfferedCourse } from "./OfferedCourse.interface";
 import { OfferedCourse } from "./OfferedCourse.model";
+import { AcademicFaculty } from "../academicFaculty/academicFaculty.model";
+import { AcademicDepartment } from "../academicDepartment/academicDepartment.model";
+import { Course } from "../Course/course.model";
+import { Faculty } from "../Faculty/faculty.model";
 
 const createOfferedCourseIntoDB=async(payload:TOfferedCourse)=>{
     const {
@@ -13,7 +20,77 @@ const createOfferedCourseIntoDB=async(payload:TOfferedCourse)=>{
         startTime,
         endTime,
       } = payload;
-    
+     //check if the semester registration id is exists!
+  const isSemesterRegistrationExits =
+  await SemesterRegistration.findById(semesterRegistration);
+
+if (!isSemesterRegistrationExits) {
+  throw new AppError(
+    httpStatus.NOT_FOUND,
+    'Semester registration not found !',
+  );
+}
+
+const academicSemester = isSemesterRegistrationExits.academicSemester;
+
+const isAcademicFacultyExits =
+  await AcademicFaculty.findById(academicFaculty);
+
+if (!isAcademicFacultyExits) {
+  throw new AppError(httpStatus.NOT_FOUND, 'Academic Faculty not found !');
+}
+
+const isAcademicDepartmentExits =
+  await AcademicDepartment.findById(academicDepartment);
+
+if (!isAcademicDepartmentExits) {
+  throw new AppError(httpStatus.NOT_FOUND, 'Academic Department not found !');
+}
+
+const isCourseExits = await Course.findById(course);
+
+if (!isCourseExits) {
+  throw new AppError(httpStatus.NOT_FOUND, 'Course not found !');
+}
+
+const isFacultyExits = await Faculty.findById(faculty);
+
+if (!isFacultyExits) {
+  throw new AppError(httpStatus.NOT_FOUND, 'Faculty not found !');
+}
+
+// check if the department is belong to the  faculty
+const isDepartmentBelongToFaculty = await AcademicDepartment.findOne({
+  _id: academicDepartment,
+  academicFaculty,
+});
+
+if (!isDepartmentBelongToFaculty) {
+  throw new AppError(
+    httpStatus.BAD_REQUEST,
+    `This ${isAcademicDepartmentExits.name} is not  belong to this ${isAcademicFacultyExits.name}`,
+  );
+}
+
+// check if the same offered course same section in same registered semester exists
+
+const isSameOfferedCourseExistsWithSameRegisteredSemesterWithSameSection =
+  await OfferedCourse.findOne({
+    semesterRegistration,
+    course,
+    section,
+  });
+
+if (isSameOfferedCourseExistsWithSameRegisteredSemesterWithSameSection) {
+  throw new AppError(
+    httpStatus.BAD_REQUEST,
+    `Offered course with same section is already exist!`,
+  );
+}
+
+
+
+
     const result =await OfferedCourse.create(payload)
     return result
 }
