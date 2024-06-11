8d28240eb9db41b298c3a3ce2be3ae839242adcb
AALabonya <1aklimakter@gmail.com>
2024-06-11 22:02:24 +0600

    added

--- src/app/modules/Auth/auth.route.ts
+++ src/app/modules/Auth/auth.route.ts
@@ -1,10 +1,11 @@
 import express from 'express';
 import auth from '../../middlewares/auth';
-import validateRequest from '../../middlewares/validateRequest';
+
 
 import { AuthControllers } from './auth.controller';
 import { AuthValidation } from './auth.validation';
 import { USER_ROLE } from '../users/user.constant';
+import validateRequest from '../../middlwares/validateRequest';
 
 const router = express.Router();
 
